c6daf2d78ac03140f42a87ca1b2cf1cf7df0da4c
Klali-Firas <firasklali1234@gmail.com>
2023-09-28 19:31:56 +0100

    ex7

---
+++ ex7.php
@@ -0,0 +1,10 @@
+<?php
+$a=7;
+$b=5;
+$c=$a<=>$b;
+switch($c){
+    case -1:echo $a." est inferieure a ".$b;break;
+    case 0:echo $a." est egale a ".$b;break;
+    case 1:echo $a." est superiere a ".$b;break;
+}
+?>
